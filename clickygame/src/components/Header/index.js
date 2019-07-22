9ffe91e223a525557e691978401700aaf8e3bb8b
Dan Longo <danlongo19@gmail.com>
2019-07-22 15:56:01 -0500

    monday

--- clickygame/src/components/Header/index.js
+++ clickygame/src/components/Header/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./style.css";
 
-function Header(props) {
-  return <h1 className="header">{props.children}</h1>;
-}
+// function Header(props) {
+//   return <h1 className="header">{props.children}</h1>;
+// }
+
+const Header = props => (
+  <div className="header">
+    <div className="title">{props.children}</div>
+    <div className="scores">
+      Score: {props.score} Highscore: {props.highscore}
+    </div>
+  </div>
+);
 
 export default Header;
